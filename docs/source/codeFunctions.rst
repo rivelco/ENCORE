4eed039fe8ff08537d75251815391a32594ba731
Ricardo VC <16997545+rivelco@users.noreply.github.com>
2025-01-13 16:22:48 -0600

    Updated documentation
    Improved and more complete documentation for the current version

---
+++ docs/source/codeFunctions.rst
@@ -0,0 +1,9 @@
+Code functions
+==============
+
+The `MainWindow` object is located in the `main.py` file. This object is the main window that opens when you run `pyhon main.py`. Here is every function that constitutes that window and how the data is processed by each function.
+
+.. automodule:: main
+   :members:
+   :undoc-members:
+   :show-inheritance:
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,10 +6,14 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import pathlib
+import sys
+sys.path.insert(0, pathlib.Path(__file__).parents[2].resolve().as_posix())
+
 project = 'EnsemblesGUI'
 copyright = '2024, rivelco'
 author = 'rivelco'
-release = '0.1'
+release = '1.0-alpha'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -18,12 +22,13 @@ extensions = [
     'sphinx.ext.duration',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
+    'sphinx_copybutton',
 ]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-
+html_static_path = ['_static']
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
